4d1f5512499dabf4c91f26c0a076bd0045645700
Halenina <galenina20032101@gmail.com>
2024-05-22 02:17:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "../Layout/Layout";
+import Layout from "./components/Layout/Layout";
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
 const MovieDetailsPage = lazy(() =>
-  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
 );
 const NotFoundPage = lazy(() =>
-  import("../../pages/NotFoundPage/NotFoundPage")
+  import("./pages/NotFoundPage/NotFoundPage")
 );
-const MovieCast = lazy(() => import("../../components/MovieCast/MovieCast"));
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
 const MovieReviews = lazy(() =>
-  import("../../components/MovieReviews/MovieReviews")
+  import("./components/MovieReviews/MovieReviews")
 );
 
 export default function App() {
