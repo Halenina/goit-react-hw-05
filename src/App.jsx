a8012b996ed9667d8f0268473bb6a9a4371c86fe
Halenina <galenina20032101@gmail.com>
2024-05-22 02:14:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-
-const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+import Layout from "../Layout/Layout";
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
 const MovieDetailsPage = lazy(() =>
-  import("./pages/MovieDetailsPage/MovieDetailsPage")
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
 );
 const NotFoundPage = lazy(() =>
-  import("./pages/NotFoundPage/NotFoundPage")
+  import("../../pages/NotFoundPage/NotFoundPage")
 );
-const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieCast = lazy(() => import("../../components/MovieCast/MovieCast"));
 const MovieReviews = lazy(() =>
-  import("./components/MovieReviews/MovieReviews")
+  import("../../components/MovieReviews/MovieReviews")
 );
 
 export default function App() {
   return (
     <Layout>
-       <Routes>
+      <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
@@ -27,5 +27,5 @@ export default function App() {
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Layout>
-  )
+  );
 }
